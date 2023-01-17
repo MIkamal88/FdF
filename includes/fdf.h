f542801b0d11de9b7821db06cc188fd3bece1d69
Mohamed I. Kamal <109902742+MIkamal88@users.noreply.github.com>
2023-01-17 14:47:22 +0400

    Update fdf.h
    Created lines

--- includes/fdf.h
+++ includes/fdf.h
@@ -17,4 +17,12 @@
 # include "../libft/includes/get_next_line.h"
 # include "../mlx_linux/mlx.h"
 
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
 #endif
